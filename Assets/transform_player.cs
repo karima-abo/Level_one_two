4238b757a55aa25051740fb510d4a751b224a9c5
karima-abo <130106968+karima-abo@users.noreply.github.com>
2023-04-28 22:20:26 +0200

    platform_moving

---
+++ Assets/transform_player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transform_player : MonoBehaviour
+{
+    public Transform posA, posB;
+    public int speed;
+    Vector2 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, posA.position) < 0.1f) { targetPos = posB.position; }
+        if (Vector2.Distance(transform.position, posB.position) < 0.1f) { targetPos = posA.position; }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+      
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
